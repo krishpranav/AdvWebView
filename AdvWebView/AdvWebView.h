f29347ca2793dc84a6558b2eb313fe6926e7e06b
Krisna Pranav <68631244+krishpranav@users.noreply.github.com>
2024-07-15 20:01:26 +0530

    Initial Commit

---
+++ AdvWebView/AdvWebView.h
@@ -0,0 +1,18 @@
+//
+//  AdvWebView.h
+//  AdvWebView
+//
+//  Created by Krisna Pranav on 15/07/24.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for AdvWebView.
+FOUNDATION_EXPORT double AdvWebViewVersionNumber;
+
+//! Project version string for AdvWebView.
+FOUNDATION_EXPORT const unsigned char AdvWebViewVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <AdvWebView/PublicHeader.h>
+
+
